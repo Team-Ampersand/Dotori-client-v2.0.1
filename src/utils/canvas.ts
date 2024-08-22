a7655f7f1ba2e56f7e9f1540ec6837eff1fd0dea
민우석 <s23044@gsm.hs.kr>
2024-08-22 20:44:40 +0900

    update canvas

--- src/utils/canvas.ts
+++ src/utils/canvas.ts
@@ -23,56 +23,56 @@ export function rotateSize(width: number, height: number, rotation: number) {
 }
 
 export async function getCroppedImg(
-  imageSrc: string,
-  rotation: 0,
-  pixelCrop?: Area,
-) {
-  const image = await createImage(imageSrc);
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
+    imageSrc:string,
+    rotation: 0,
+    pixelCrop?:Area,
+  ) {
+    const image = await createImage(imageSrc);
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
 
-  if (!pixelCrop) return null;
-  if (!ctx) return null;
+    if(!pixelCrop) return null;
+    if (!ctx) return null;
+  
+    const rotRad = getRadianAngle(rotation);
+  
+    const { width: bBoxWidth, height: bBoxHeight } = rotateSize(
+      image.width,
+      image.height,
+      rotation
+    );
+  
+    canvas.width = bBoxWidth;
+    canvas.height = bBoxHeight;
+  
+    ctx.translate(bBoxWidth / 2, bBoxHeight / 2);
+    ctx.rotate(rotRad);
+    ctx.scale(1, 1);
+    ctx.translate(-image.width / 2, -image.height / 2);
+  
+    ctx.drawImage(image, 0, 0);
+  
+    const data = ctx.getImageData(
+      pixelCrop.x,
+      pixelCrop.y,
+      pixelCrop.width,
+      pixelCrop.height
+    );
+  
+    canvas.width = pixelCrop.width;
+    canvas.height = pixelCrop.height;
+  
+    ctx.putImageData(data, 0, 0);
 
-  const rotRad = getRadianAngle(rotation);
-
-  const { width: bBoxWidth, height: bBoxHeight } = rotateSize(
-    image.width,
-    image.height,
-    rotation,
-  );
-
-  canvas.width = bBoxWidth;
-  canvas.height = bBoxHeight;
-
-  ctx.translate(bBoxWidth / 2, bBoxHeight / 2);
-  ctx.rotate(rotRad);
-  ctx.scale(1, 1);
-  ctx.translate(-image.width / 2, -image.height / 2);
-
-  ctx.drawImage(image, 0, 0);
-
-  const data = ctx.getImageData(
-    pixelCrop.x,
-    pixelCrop.y,
-    pixelCrop.width,
-    pixelCrop.height,
-  );
-
-  canvas.width = pixelCrop.width;
-  canvas.height = pixelCrop.height;
-
-  ctx.putImageData(data, 0, 0);
-
-  return new Promise<Blob>((resolve, reject) => {
-    canvas.toBlob((blob) => {
-      if (blob) {
-        const extension = blob.type.split('/')[1];
-        const file = new File([blob], `profile.${extension}`, {
-          type: blob.type,
-        });
-        return resolve(file);
-      } else reject(new Error('Canvas is empty'));
+    return new Promise<Blob>((resolve, reject) => {
+      canvas.toBlob((blob) => {
+        if (blob) {
+          const extension = blob.type.split('/')[1];
+          const file = new File([blob], `profile.${extension}`, {
+            type: blob.type,
+          });
+          return resolve(file);
+        } else reject(new Error('Canvas is empty'));
+      });
     });
-  });
-}
+  }
